7ace85150d410e34f81307812f31b2f6623307e0
Joe <joe.ekiert@gmail.com>
2015-01-31 22:17:03 -0800

    Made the change away from IIFE to just a constructor, can also now take options object to set the namespace and specific table
    - eh, people speak against using 'new' but for this, it was just easier
    - if I want a more sophisticated store api, I won't use localStorage anyway

--- JocalStorage.js
+++ JocalStorage.js
@@ -1,40 +1,34 @@
-var JocalStorage = (function (_, win) {
+var JocalStorage = function (options) {
   var cache = [],
-    properties = {},
-    namespace = "JOE";
+      properties = {},
+      options = options || {},
+      namespace = options.namespace || "JOE",
+      table = options.table || "cache";
 
   // Functions to encapsulate questionable FireFox 3.6.13 behavior
   // when about.config::dom.storage.enabled === false
   // See https://github.com/marcuswestin/store.js/issues#issue/13
-  var _isLocalStorageSupported = function () {
-    try { return ('localStorage' in win && win['localStorage']); }
+  var isLocalStorageSupported = function () {
+    try { return ('localStorage' in window && window['localStorage']); }
     catch (err) { return false; }
   };
 
-  var _initialize = function (ns) {
-    namespace = ns || namespace;
-    _loadCache();
-    // JOE: have some sort of initialized flag
-    // don't like how user can still access _loadCache despite an uninitialized state
-    // or perhaps vocalize that if uninitialized, the namespace is set to JOE by default
-  };
-
-  var _loadCache = function () {
-    if (_isLocalStorageSupported()) {
-      cache = JSON.parse(localStorage.getItem(namespace + ":cache")) || [];
-      properties = JSON.parse(localStorage.getItem(namespace + ":properties")) || {};
+  var loadCache = function () {
+    if (isLocalStorageSupported()) {
+      cache = JSON.parse(localStorage.getItem(namespace + ":" + table)) || [];
+      properties = JSON.parse(localStorage.getItem(namespace + ":" + table + ":properties")) || {};
       properties.lastId = properties.lastId || 0;
     }
   };
 
-  var _digest = function () {
-    if (_isLocalStorageSupported()) {
-      localStorage.setItem(namespace + ":cache", JSON.stringify(cache));
-      localStorage.setItem(namespace + ":properties", JSON.stringify(properties));
+  var digest = function () {
+    if (isLocalStorageSupported()) {
+      localStorage.setItem(namespace + ":" + table, JSON.stringify(cache));
+      localStorage.setItem(namespace + ":" + table + ":properties", JSON.stringify(properties));
     }
   };
 
-  var _get = function (id) {
+  var get = function (id) {
     var result = false;
     for (var i = 0; i < cache.length; i++) {
       if (cache[i].id === id) {
@@ -42,69 +36,75 @@ var JocalStorage = (function (_, win) {
       }
     }
     return result;
-  };
+  }
 
-  var _store = function (obj) {
+  var store = function (obj) {
     // JOE: doesn't really do anything ... should I have the test revolve around id?
     if (!_.contains(cache, obj)) {
       obj.id = ++properties.lastId;
       cache.push(obj);
-      _digest();
+      digest();
       return obj.id;
     }
     return false;
   };
 
-  var _removeFromCache = function (id) {
+  var removeFromCache = function (id) {
     var removed = _.remove(cache, function (obj) {
       return obj.id === id;
     });
 
     if (removed.length > 0) {
-      _digest();
-      _loadCache();
+      digest();
+      loadCache();
       return removed;
     }
 
     return false;
   };
 
-  var _nuke = function () {
+  var nuke = function () {
     cache = [];
-    properties = {};
-    localStorage.clear();
-    _digest();
+    properties.lastId = 0;
+    //localStorage.clear();
+    digest();
   };
 
   return {
-    init: function (namespace) {
-      _initialize(namespace);
+    init: function () {
+      loadCache();
     },
     store: function (arg) {
       var result;
       if (Array.isArray(arg)) {
         result = [];
         for (var i = 0; i < arg.length; i++) {
-          result.push(_store(arg[i]));
+          result.push(store(arg[i]));
         }
       }
       else {
-        result = _store(arg);
+        result = store(arg);
       }
 
       return result;
     },
     get: function (id) {
       if (id) {
-        return _get(id);
+        return get(id);
       }
       return cache;
     },
     remove: function (id) {
-      _removeFromCache(id);
+      removeFromCache(id);
     },
     clear: function () {
-      _nuke();
+      nuke();
+    },
+
+    debug: function () {
+      console.log("JOE: properties: ", properties);
+      console.log("JOE: cache: ", cache);
     }
   };
-}(_, window));
+
+};
